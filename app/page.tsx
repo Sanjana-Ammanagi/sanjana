5ee4dad20a4f03a27fd71a9181dca769b5bc4471
= <=>
2023-10-31 19:04:24 +0530

    converted to tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,28 @@
 import Image from "next/image";
-import cat from "../public/cat_portfolio.jpg";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div
-        style={{
-          margin: "60px",
-          border: "5px solid black",
-          textAlign: "center",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
+      <div className="m-12 border-5 border-black text-center">
+        <div className="flex items-center justify-center">
           <Image
-            src={cat}
+            src="/cat_portfolio.jpg"
             alt="profile"
             width={140}
             height={80}
-            layout="intrinsic"
           />
         </div>
-        <p style={{ fontSize: "30px" }}>
-          <b></b>Sanjana Ammanagi<b></b>
+        <p className="text-3xl">
+          <b>Sanjana Ammanagi</b>
         </p>
         <Link href="https://www.linkedin.com/in/sanjana-ammanagi/">
-          <p
-            style={{
-              display: "inline-block",
-              margin: "0 10px",
-              color: "blue",
-            }}
-          >
-            LinkedIn
-          </p>
+          <p className="inline-block m-4 text-blue-500">LinkedIn</p>
         </Link>
         <Link href="https://leetcode.com/Sanjana250503/">
-          <p
-            style={{ display: "inline-block", margin: "0 10px", color: "blue" }}
-          >
-            LeetCode
-          </p>
+          <p className="inline-block m-4 text-blue-500">LeetCode</p>
         </Link>
-        <p style={{ textAlign: "center", color: "Red 400" }}>
+        <p className="text-red-400">
           I am a seasoned Software Development Engineer with strong proficiency
           in both Java and Python. My experience in software development spans
           various domains, allowing me to tackle a broad spectrum of projects
